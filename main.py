cf920d72837f392cc76f8a393047d3821583f30d
v_lang <31761812+Shahnawaz-lang@users.noreply.github.com>
2022-12-28 17:05:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-# This example requires the 'message_content' privileged intents
-
 import os
 import discord
 from discord.ext import commands
@@ -8,19 +6,19 @@ from help_func import formatter
 
 
 intents = discord.Intents.all()
-bot = commands.Bot(command_prefix='!', intents=intents)
+bot = commands.Bot(command_prefix='%', intents=intents)
 
 
 @bot.command()
 async def parse_sentence(ctx, model, string):
-#     embed = discord.Embed(title=string, colour=discord.Colour.blue())
-#     output_string = formatter(request('GET', url=url, params={'model': str(model).lower() or "english",
-#                                                               'tokenizer': '',
-#                                                               'parser': '',
-#                                                               'tagger': '',
-#                                                               'data': string}).json()['result'])
-#     for k, v in output_string.items():
-#         embed.add_field(name=k, value=v, inline=False)
+    embed = discord.Embed(title=string, colour=discord.Colour.blue())
+    output_string = formatter(request('GET', url=url, params={'model': str(model).lower() or "english",
+                                                              'tokenizer': '',
+                                                              'parser': '',
+                                                              'tagger': '',
+                                                              'data': string}).json()['result'])
+    for k, v in output_string.items():
+        embed.add_field(name=k, value=v, inline=False)
     await ctx.send(model)
 
 
