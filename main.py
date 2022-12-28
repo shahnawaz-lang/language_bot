f47a23be1a01c700718f7ea8a69d7ff7122ee629
v_lang <31761812+Shahnawaz-lang@users.noreply.github.com>
2022-12-28 17:02:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -13,15 +13,15 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 
 @bot.command()
 async def parse_sentence(ctx, model, string):
-    embed = discord.Embed(title=string, colour=discord.Colour.blue())
-    output_string = formatter(request('GET', url=url, params={'model': str(model).lower() or "english",
-                                                              'tokenizer': '',
-                                                              'parser': '',
-                                                              'tagger': '',
-                                                              'data': string}).json()['result'])
-    for k, v in output_string.items():
-        embed.add_field(name=k, value=v, inline=False)
-    await ctx.send(embed=embed)
+#     embed = discord.Embed(title=string, colour=discord.Colour.blue())
+#     output_string = formatter(request('GET', url=url, params={'model': str(model).lower() or "english",
+#                                                               'tokenizer': '',
+#                                                               'parser': '',
+#                                                               'tagger': '',
+#                                                               'data': string}).json()['result'])
+#     for k, v in output_string.items():
+#         embed.add_field(name=k, value=v, inline=False)
+    await ctx.send(model)
 
 
 
